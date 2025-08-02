84a0723c8f6922ab831ab235d9579d98cf09dda2
Tatyana <filippovat42@gmail.com>
2025-08-02 13:40:23 +0300

    компонент Карточка услуги

--- src/shared/components/ServiceCards/ServiceCards/ServiceCards.tsx
+++ src/shared/components/ServiceCards/ServiceCards/ServiceCards.tsx
@@ -1,24 +1,49 @@
 import Card from "@/shared/components/ServiceCards/Card/Card";
 import HeadingSection from "@/shared/components/HeadingSection/HeadingSection";
+import styles from "./styles.module.scss";
 
 const ServiceCards = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <HeadingSection
         title="Какие проблемы мы решаем"
         description="Неважно, попали ли вы в ДТП, сломались на трассе или просто хотите перегнать автомобиль — мы обеспечим безопасную транспортировку с гарантией сохранности."
       />
-      <Card
-        titleCard={"Эвакуация легковых автомобилей"}
-        descriptionCard={
-          "Эвакуируем легковой автомобиль или легкий кроссовер массой до 2.5.тонн"
-        }
-        descriptionSecondCard={
-          "В данную категорию относятся хетчбеки, седаны, универсалы, минивены"
-        }
-        imageCard="/imageCard.png"
-        size={"S"}
-      />
+      <div className={styles.containerCards}>
+        <Card
+          titleCard={"Эвакуация легковых автомобилей"}
+          descriptionCard={
+            "Эвакуируем легковой автомобиль или легкий кроссовер массой до 2.5.тонн"
+          }
+          descriptionSecondCard={
+            "В данную категорию относятся хетчбеки, седаны, универсалы, минивены"
+          }
+          imageCard="/imageCard.png"
+          size={"M"}
+        />
+        <Card
+          titleCard={"Эвакуация легковых автомобилей"}
+          descriptionCard={
+            "Эвакуируем легковой автомобиль или легкий кроссовер массой до 2.5.тонн"
+          }
+          descriptionSecondCard={
+            "В данную категорию относятся хетчбеки, седаны, универсалы, минивены"
+          }
+          imageCard="/imageCard.png"
+          size={"S"}
+        />
+        <Card
+          titleCard={"Эвакуация легковых автомобилей"}
+          descriptionCard={
+            "Эвакуируем легковой автомобиль или легкий кроссовер массой до 2.5.тонн"
+          }
+          descriptionSecondCard={
+            "В данную категорию относятся хетчбеки, седаны, универсалы, минивены"
+          }
+          imageCard="/imageCard.png"
+          size={"L"}
+        />
+      </div>
     </div>
   );
 };
